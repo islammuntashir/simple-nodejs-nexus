22e24a028d67a521f0bfec74b3dd15aff87b6860
Muntashir Islam <muntashir.islam@hishab.co>
2021-07-16 13:02:55 -0600

    Add docker push

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-(
-  cd "$DIR/.." # Go to project dir.
-
-  ssh ubuntu@3.222.207.247 -i { secrets.PRIVKEY } -o StrictHostKeyChecking=no <<-EOF
-    cd /home/ubuntu/code/simple-node-nuxus
-    git pull
-    docker-compose pull
-    docker-compose stop
-    docker-compose rm -f
-    docker-compose -f docker-compose.yml up -d
-EOF
-)
+git pull
+docker-compose pull
+docker-compose stop
+docker-compose rm -f
+docker-compose -f docker-compose.yml up -d
